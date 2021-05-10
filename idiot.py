a00b13f3fbe1d449d46f4890fe58bb2f7ca6f534
Rishabh Anand <thefenrislycaon@gmail.com>
2021-05-10 18:49:56 +0530

    idiot

--- idiot.py
+++ idiot.py
@@ -2,4 +2,5 @@ while True:
     prompt = input('Want to know how to keep an idiot busy for hours?\n')
     if 'n' in prompt:
         print('You figured it out huh ?')
+        input("Press any key to exit...")
         exit()
